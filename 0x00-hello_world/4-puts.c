f157cca9e06136b8c563f1c440f0c10aef9277ac
Lydiah <lydiahcedric@gmail.com>
2022-12-13 04:12:17 -0800

    fix errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+ 
+/*
+ * main - Entry point
+ *
+ * retrn 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
