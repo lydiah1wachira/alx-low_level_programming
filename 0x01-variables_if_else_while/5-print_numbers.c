a4d7591698db8d942ee06c0c0ff1f0efe28d3b70
Lydiah <lydiahcedric@gmail.com>
2022-12-22 14:28:08 -0800

    print numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d\n", number)
+	}
+
+	return (0);
+}
