019837c400806988433c0b355e5d7900593bbe65
Lydiah <lydiahcedric@gmail.com>
2022-12-22 13:52:22 -0800

    remove white spaces.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point 
+ * main-Entry point 
  * Return:0
  */
 int main(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
+
 	c = 'A';
+
 	while (c <= 'Z')
 	{
 		putchar(c);
