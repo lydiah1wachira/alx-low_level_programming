514b8a347a7203a8301291aa48db830a628e60b2
Lydiah <lydiahcedric@gmail.com>
2022-12-30 15:20:34 -0800

    program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Description: program to print all possible
+ * main - entry point 
+ * Description: program  to print all possible 
  * combinations of two two-digit numbers
  *
  * Return: 0
@@ -22,12 +22,14 @@ int main(void)
 				putchar(f / 10 + 48);
 				putchar(f % 10 + 48);
 
-				if (i * 100 + j 
-						
-						
-						
-						!= 9899)
+				if (e * 100 + f != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
+	putchar('\n');
+	return (0);
 }
