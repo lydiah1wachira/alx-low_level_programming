a4805f89295b8effe31e8f6fc6bca93501016000
Lydiah <lydiahcedric@gmail.com>
2022-12-30 15:22:38 -0800

    remove trailing whitespaces.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point 
- * Description: program  to print all possible 
+ * main - entry point
+ * Description: program to print all possible
  * combinations of two two-digit numbers
  *
  * Return: 0
