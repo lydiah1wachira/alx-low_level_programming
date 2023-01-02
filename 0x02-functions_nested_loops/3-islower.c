b51542a8f17fc0cdd0cbdc852a34fbf64aa88747
Lydiah <lydiahcedric@gmail.com>
2023-01-02 09:45:20 -0800

    check for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_slower - checks for lower case characters
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
