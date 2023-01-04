f37998a3498cd76b852b6e45ecf1b7ed1c7f437a
Lydiah <lydiahcedric@gmail.com>
2023-01-04 13:39:17 -0800

    102-magic

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+
+}
