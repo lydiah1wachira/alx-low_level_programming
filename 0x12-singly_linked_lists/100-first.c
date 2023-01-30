c3fab533bd6711aea3329cbeab652d5126fda4a6
Lydiah <lydiahcedric@gmail.com>
2023-01-31 02:57:30 +0300

    fix errors.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */ int main(void) {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
